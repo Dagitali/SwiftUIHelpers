a7f59ed54631cee8b9a3e9752657645145f0b3aa
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2024-12-31 14:44:53 -0500

    style(ui): Update *Tests.swift
    Standardize how license comment is formatted.

--- Tests/SwiftUIHelpersTests/Protocols/AppThemeTests.swift
+++ Tests/SwiftUIHelpersTests/Protocols/AppThemeTests.swift
@@ -20,7 +20,7 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
- */
+*/
 
 /// Define a default theme for testing purposes.
 struct DefaultAppTheme: AppTheme {}
