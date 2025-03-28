53a69b2f9060751d14c16217ec679bd8b60c36d9
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-03-28 07:38:44 -0400

    feat(image): Create ImageExtensions.swift

---
+++ Sources/SwiftUIHelpers/Extensions/ImageExtensions.swift
@@ -0,0 +1,30 @@
+//
+//  ImageExtensions.swift
+//  SwiftUIHelpers
+//
+//  Copyright © 2025 Dagitali LLC. All rights reserved.
+//
+
+/*
+ See the LICENSE.txt file for this package’s licensing information.
+
+ Abstract:
+ Helper extensions for working with `Image` types.
+*/
+
+import SwiftUI
+
+// MARK: - Internal Extensions
+
+@available(iOS 18.0, macOS 15.0, tvOS 18.0, visionOS 2.0, watchOS 11.0, *)
+public extension Image {
+    // MARK: Initializers
+
+    init(image: PlatformImage) {
+#if canImport(UIKit)
+        self.init(uiImage: image)
+#elseif canImport(AppKit)
+        self.init(nsImage: image)
+#endif
+    }
+}
